2b10537019120c5dac5e5abad02c79d552327c31
agustinaduric <agustinaduric@gmail.com>
2025-04-22 14:04:46 +0000

    eliminar CargarConfig anterior

--- utils/config/funciones_config.go
+++ utils/config/funciones_config.go
@@ -1,25 +1,8 @@
 package config
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 )
-
-// ** CargarConfig**
-// recibe el path de .json segun el modulo y un puntero al struct para cargarle los datos
-func CargarConfig[T any](path string, cfg *T) error { //Comentario de nardo: para q pinga esta ese cfg
-	file, err := os.Open(path)
-	if err != nil {
-		log.Printf("error")
-	}
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	return decoder.Decode(file)
-	//jsonParser.Decode(&config)
-	//jsonParser := json.NewDecoder(configFile)
-}
 
 func IntToStringConPuntos(valor int) string {
 	return fmt.Sprintf(":%d", valor)
