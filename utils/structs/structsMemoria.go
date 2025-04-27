9697566a50250415daccbef4ae74ab4c98073759
Nardo <fnardelli@frba.utn.edu.ar>
2025-04-27 17:33:47 -0300

    Update structsMemoria.go

--- utils/structs/structsMemoria.go
+++ utils/structs/structsMemoria.go
@@ -3,24 +3,24 @@ package structs
 type ProcesoMemoria struct {
 	PID          int
 	Tamanio      int
-	//TablaPaginas *TablaDePaginas
+	TablaDePaginas *TablaDePaginas
 	EnSwap       bool
 	Metricas     MetricasMemoria
-}
-/*
-type EntradaTablaDePaginas struct {
-	Presente       bool            //Cambiar nombre por alguno mejor, no me gusta
-	Frame          int             // numero de marco si es ultimo nivel
-	TablaSiguiente *TablaDePaginas // punterovich a siguiente nivel si no es ultimo
+	Path	     string
+	Instrucciones string
 }
 
 type TablaDePaginas struct {
-	Entradas []*EntradaTablaDePaginas
+    NivelDeTabla int 	
+    PaginaMarco []PaginaMarco		//		
+    SiguienteNivel   *TablaDePaginas  // puntero a la tabla del siguiente nivel
 }
 
-Se nota que no estudie la teoria, revisar mas tarde
+type PaginaMarco struct {
+	Pagina int
+	Marco int
+}
 
-*/
 var MemoriaUsuario []byte
 
 type MetricasMemoria struct {
